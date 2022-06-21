9344f7543c7bf53808d6b788b89c441505934f5b
d4rk01 <98687567+d4rk01@users.noreply.github.com>
2022-06-21 14:19:50 +0300

    Delete MSI Afterburner directory

--- MSI Afterburner/SDK/Samples/Plugins/Monitoring/CPU/CPU.h
+++
@@ -1,5 +0,0 @@
-#ifdef CPU_EXPORTS
-#define CPU_API extern "C" __declspec(dllexport)
-#else
-#define CPU_API extern "C" __declspec(dllimport)
-#endif
--- MSI Afterburner/SDK/Samples/Plugins/Monitoring/GPU/GPU.h
+++
@@ -1,5 +0,0 @@
-#ifdef GPU_EXPORTS
-#define GPU_API extern "C" __declspec(dllexport)
-#else
-#define GPU_API extern "C" __declspec(dllimport)
-#endif
